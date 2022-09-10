73e04da7871e1cafa012bd8b509b3c7c11ddc687
sergiohdljr <sergio.tjf1@gmail.com>
2022-09-10 19:19:07 -0300

    string pt1

---
+++ Strings/iniciandoStrings.js
@@ -0,0 +1,10 @@
+ let str1 = "minha string bacanuda"
+
+//  console.log(str1.replace("i","o"))
+//  console.log(str1);
+
+//  console.log(str1.indexOf("string"))
+//  console.log(str1.includes("minha"))
+
+console.log(str1.slice(2,5))
+console.log(str1.substring(2, 5));
