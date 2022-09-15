b18a31b0214a40248cc00202f171d276c8d0d710
sergiohdljr <sergio.tjf1@gmail.com>
2022-09-15 17:41:43 -0300

    Finalizando Date

---
+++ Date/date.js
@@ -0,0 +1,15 @@
+const data = new Date()
+console.log(data)
+console.log(data.getDay())
+console.log(data.getDate());
+console.log(data.getMonth());
+console.log(data.getTime());
+console.log(data.getFullYear());
+console.log(data.getHours());
+console.log(data.getUTCHours());
+
+console.log(data.toString()) //transformar Data string
+
+console.log(data.toDataString());
+console.log(data.toISOString());
+console.log(data.toLocaleDateString());
