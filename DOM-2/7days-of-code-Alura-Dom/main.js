2e9b5f50b6ff17521f4ec02974c312d8c30cef1e
sergiohdljr <sergio.tjf1@gmail.com>
2022-09-17 20:15:50 -0300

    adicionando dados na tabela

--- DOM-2/7days-of-code-Alura-Dom/main.js
+++ DOM-2/7days-of-code-Alura-Dom/main.js
@@ -1,13 +1,41 @@
 const nome = document.querySelector("#name");
-const dataNascimento = document.querySelector("#birth-date");
 const form = document.querySelector(".js-form");
-
 const btn = document.querySelector("button");
+const dataNascimento = document.querySelector("#birth-date");
+const tabelaPrincipal = document.querySelector(".tabela-principal");
+const tbody = tabelaPrincipal.querySelector("tbody")
 
-form.addEventListener('submit', (e)=>{
-    let dataFormatada = dataNascimento.value.split("-").reverse().join("-");
-    console.log(
-      `${nome.value} nasceu em ${dataFormatada}`
-    );
-    e.preventDefault()
-})
+let pessoas = [];
+
+form.addEventListener("submit", (e) => {
+  let nomePessoa = nome.value;
+  let dataFormatada = dataNascimento.value.split("-").reverse().join("-");
+
+  pessoas.push({nomePessoa , dataFormatada})
+  e.preventDefault();
+  renderTable()
+
+  nome.value = ""
+  dataNascimento.value = "" 
+
+});
+
+function criarRow(pessoa) {
+  const tr = document.createElement("tr");
+  const tdNome = document.createElement("td");
+  tdNome.textContent = `${pessoa.nomePessoa}`;
+  tr.appendChild(tdNome);
+  const tdData = document.createElement("td");
+  tdData.textContent = `${pessoa.dataFormatada}`;
+  tr.appendChild(tdData);
+
+  return tr
+}
+
+function renderTable(){
+  tbody.innerHTML = "";
+  pessoas.forEach((pessoa) => {
+     tbody.appendChild(criarRow(pessoa));
+});
+
+}
